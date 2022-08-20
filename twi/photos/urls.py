97f913d7da55a8479c8b83800d7220ce3bdd65ab
Ruslan <safaev.rus@gmail.com>
2022-08-20 15:40:55 +0300

    some new features

--- twi/photos/urls.py
+++ twi/photos/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'photos'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    # ex: /polls/5/
-    # path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /photos/5/
+    path('user/<int:user_id>/', views.detail, name='detail'),
 ]
--- twi/photos/views.py
+++ twi/photos/views.py
@@ -1,13 +1,37 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.utils.regex_helper import Choice
 
 from .models import User
 
 
 def index(request):
     users = User.objects.all()
-    template = loader.get_template('photos/index.html')
-    context = {
-        'user_information': users,
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, template_name='photos/index.html',
+                  context={'user_information': users})
+
+
+def detail(request, user_id):
+    # user = User.objects.get(pk=user_id)
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, template_name='photos/detail.html', context={'user': user})
+
+
+def some(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    try:
+        selected_choice = user.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'photo/detail.html', {
+            'question': user,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('photo:results', args=(user.id,)))
